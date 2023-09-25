a3fe270e289fc43ece119c9f2e15cfb3fb2a0937
Liberty <145721240+Liberty945@users.noreply.github.com>
2023-09-25 18:13:23 +0300

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'), #перенаправление views
+    path('', views.index, name='home'), #перенаправление в views
 ]
